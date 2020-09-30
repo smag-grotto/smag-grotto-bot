1a59bead41d687cb804fdbba00fc3535f7d15d70
Jake Ruston <rjake@hotmail.co.uk>
2020-09-30 18:59:03 +0100

    Added info command

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ The Discord bot for Smag Grotto.
 ```
 .smag register
 ```
+
+## To do
+
+- Trigger event on `userUpdate` so registered users on the website have their name automatically updated.
+- Trigger event on `userUpdate` so registered users on the website have their picture automatically updated.
+- Create command to set the user's bio: `.smag bio <bio>`
--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ client.on('message', message => {
 
   if (command === 'register') {
     const name = message.author.username;
-    const picture = message.author.avatarURL({ format: 'png', size: 256 });
+    const picture = message.author.avatarURL({ format: 'png', size: 128 });
 
     fetch(`http://${process.env.API_HOST}:${process.env.API_PORT}/members`, {
       method: 'POST',
@@ -27,6 +27,22 @@ client.on('message', message => {
         if (res.status === 400) return message.channel.send('**Error:** You are already registered');
 
         return message.channel.send(`You are now registered. Check yourself out here: http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/members`);
+      })
+      .catch(err => message.channel.send(`Failed to connect to the API.\n\`\`\`${err.message}\`\`\``));
+  }
+
+  if (command === 'info') {
+    const name = message.author.username;
+
+    fetch(`http://${process.env.API_HOST}:${process.env.API_PORT}/members/${name}`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(async res => [res, await res.json()])
+      .then(([res, json]) => {
+        if (res.status === 404) return message.channel.send('**Error:** You need to register with \`.smag register\`');
+
+        return message.channel.send(`**Username:** ${json.name}\n**Picture:** <${json.picture}>\n**Bio:** ${json.bio}`);
       })
       .catch(err => message.channel.send(`Failed to connect to the API.\n\`\`\`${err.message}\`\`\``));
   }
