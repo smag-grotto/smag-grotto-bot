adf3b76dccf239258f62e095ff48169ff0a21faf
Jake Ruston <rjake@hotmail.co.uk>
2020-10-01 21:11:03 +0100

    Added a command to set your bio

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ client.on('message', message => {
       .then(([res, json]) => {
         if (res.status === 400) return message.channel.send('**Error:** You are already registered');
 
-        return message.channel.send(`You are now registered. Check yourself out here: http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/members`);
+        return message.channel.send(`You are now registered. Check yourself out here: http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/members/${name}`);
       })
       .catch(err => message.channel.send(`Failed to connect to the API.\n\`\`\`${err.message}\`\`\``));
   }
@@ -45,6 +45,25 @@ client.on('message', message => {
         return message.channel.send(`**Username:** ${json.name}\n**Picture:** <${json.picture}>\n**Bio:** ${json.bio}`);
       })
       .catch(err => message.channel.send(`Failed to connect to the API.\n\`\`\`${err.message}\`\`\``));
+  }
+
+  if (command === 'bio') {
+    if (!args.length) message.channel.send('**Error:** You need to supply a bio');
+
+    const name = message.author.username;
+    const bio = args.join(' ');
+
+    fetch(`http://${process.env.API_HOST}:${process.env.API_PORT}/members/${name}`, {
+      method: 'PATCH',
+      body: JSON.stringify({ bio }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(async res => [res, await res.json()])
+      .then(([res, json]) => {
+        if (res.status === 404) return message.channel.send('**Error:** You need to register with \`.smag register\`');
+
+        return message.channel.send(`Your bio has been set. Check yourself out here: http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/members/${name}`);
+      });
   }
 });
 
